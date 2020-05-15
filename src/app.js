d365a46c9d6de3dc765a938c16a3d27cec71a204
leny <info@flatland.be>
2020-05-15 16:13:25 +0200

    Prepare react app

---
+++ src/app.js
@@ -0,0 +1,17 @@
+/* leny/animalese
+ *
+ * /src/app.js - Main entry point
+ *
+ * coded by leny@BeCode
+ * started at 15/05/2020
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Root from "./components/root"
+
+ReactDOM.render(
+    <Root />,
+    document.querySelector("#app"),
+)
---
+++ src/components/root.js
@@ -0,0 +1,15 @@
+/* leny/animalese
+ *
+ * /src/components/root.js - Root Component
+ *
+ * coded by leny@BeCode
+ * started at 15/05/2020
+ */
+
+import React from "react";
+
+const Root=()=>(
+    <p>{"Hello world!"}</p>
+);
+
+export default Root;
