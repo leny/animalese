baeac5d1b7be83f5a9cb8bbc4667710616070e74
leny <info@flatland.be>
2020-05-15 16:35:48 +0200

    Prepare basic UX

--- src/components/root.js
+++ src/components/root.js
@@ -6,10 +6,35 @@
  * started at 15/05/2020
  */
 
-import React from "react";
+/* eslint-disable react/button-has-type */
 
-const Root=()=>(
-    <p>{"Hello world!"}</p>
-);
+import React, {useState, useCallback} from "react";
+
+// const AUDIO_PATH = "assets/audio";
+
+const Root = () => {
+    const [text, setText] = useState("");
+
+    const handleSubmit = useCallback(
+        e => {
+            e.preventDefault();
+
+            console.warn("Animalese:", text);
+        },
+        [text],
+    );
+
+    return (
+        <form action={"#"} onSubmit={handleSubmit}>
+            <input
+                type={"text"}
+                placeholder={"Tapez quelque chose…"}
+                value={text}
+                onChange={e => setText(e.target.value)}
+            />
+            <button type={"submit"}>{"Parler en animalese"}</button>
+        </form>
+    );
+};
 
 export default Root;
